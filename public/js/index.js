103dde15b113e638bb70fa07d81c37d3e74e6a3f
Thiago <52945005+githiago-f@users.noreply.github.com>
2024-09-25 00:25:08 +0000

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -4,19 +4,30 @@ const nomeInput = document.getElementById('nome');
 const listaDespesas = document.getElementById('despesas');
 const mensagem = document.getElementById('mensagem');
 
+// Carrega a página
 async function main() {
+    // carrega o total
     const { total } = await fetch('/despesas/total').then(res => res.json());
+    // atualiza o total das despesas e insere em BRL
     totalText.textContent = Number(total).toLocaleString('pt-br', { currency: 'BRL', style: 'currency' });
     carregaDespesas();
 }
 
 async function carregaDespesas() {
     const paginaDeDespesas = await fetch('/despesas').then(res => res.json());
-    listaDespesas.innerHTML = (paginaDeDespesas.content ?? []).map(despesa => (`
-        <li class="despesa"><span>${despesa.nome}</span>&nbsp;<span>${Number(despesa.valor).toLocaleString('pt-br', { currency: 'BRL', style: 'currency' })}</span></li>
-    `)).join('');
+
+    // atualiza a lista de despesas
+    listaDespesas.innerHTML = (paginaDeDespesas.content ?? [])
+        .map(despesa => (`
+            <li class="despesa">
+                <span>${despesa.nome}</span>&nbsp;
+                <span>${Number(despesa.valor).toLocaleString('pt-br', { currency: 'BRL', style: 'currency' })}</span>
+            </li>
+        `))
+    .join('');
 }
 
+// cria nova despesa
 document.getElementById("novaDespesa").addEventListener('submit', async (e) => {
     e.preventDefault();
     const res = await fetch('/despesas', { 
@@ -26,15 +37,15 @@ document.getElementById("novaDespesa").addEventListener('submit', async (e) => {
         },
         body: JSON.stringify({ valor: Number(valorInput.value.replace(',', '.')), nome: nomeInput.value })
     });
-
     const data = await res.json();
 
     if(res.status === 201) {
         pagina = 0;
         valorInput.value = '';
         nomeInput.value = '';
-        carregaDespesas();
         mensagem.style.display = 'none';
+        // chama a atualização da página
+        main();
         return true;
     }
 
